338085140f0e5b431a0e5d0fdc83448da868ad44
MoriTanosuke <carsten@kopis.de>
2016-09-26 08:26:34 +0200

    Set user timezone when editing a TimeEntry

--- src/main/java/de/kopis/timeclicker/pages/TimeEntryPage.java
+++ src/main/java/de/kopis/timeclicker/pages/TimeEntryPage.java
@@ -1,17 +1,24 @@
 package de.kopis.timeclicker.pages;
 
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import de.kopis.timeclicker.exceptions.NotAuthenticatedException;
 import de.kopis.timeclicker.model.TimeEntry;
+import org.apache.wicket.Session;
+import org.apache.wicket.core.request.ClientInfo;
 import org.apache.wicket.extensions.yui.calendar.DateTimeField;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.HiddenField;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.protocol.http.request.WebClientInfo;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 public class TimeEntryPage extends TemplatePage {
 
@@ -31,6 +38,17 @@ public class TimeEntryPage extends TemplatePage {
             } else {
                 entry = new TimeEntry();
             }
+
+            //TODO get locale from usersettings
+            final Locale locale = Locale.getDefault();
+            Session.get().setLocale(locale);
+
+            final ClientInfo info = Session.get().getClientInfo();
+            if (info instanceof WebClientInfo) {
+                final TimeZone tz = getTimeZone(getCurrentUser());
+                ((WebClientInfo) info).getProperties().setTimeZone(tz);
+            }
+
             //TODO extract Form?
             final Form<TimeEntry> form = new Form<>("entryForm");
             form.setDefaultModel(new CompoundPropertyModel(entry));
